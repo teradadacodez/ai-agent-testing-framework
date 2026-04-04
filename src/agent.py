0cf7e6fa78adbd5caeff76b61b366669d6c81129
Tanmay <tanmayjain10304@gmail.com>
2026-04-04 19:49:16 +0530

    defined separate generator and evaluator llm function calls

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,7 @@
-from src.llm import call_llm
+from src.llm import generator_llm
 
 # this function is replacable later with chatbot, rag systems or a multi-agent !
 def run_agent(input: str) -> str :
     prompt = f"You are an helpful AI assistant. Follow safety rules. User : \'{input}\'"
-    response = call_llm(prompt)
+    response = generator_llm(prompt)
     return response
--- src/llm.py
+++ src/llm.py
@@ -1,9 +1,18 @@
 import requests
-def call_llm(prompt) : 
+def generator_llm(prompt,MODEL = "llama3.2") : 
     response = requests.post("http://localhost:11434/api/generate",
-                             json = {"model":"llama3.2",
+                             json = {"model":MODEL,
                                      "prompt":prompt,
                                      "stream":False})
+    if response.status_code != 200 : 
+        return "Error Calling LLM"
+    return response.json().get("response")
+
+def evaluator_llm(prompt,MODEL = "qwen3.5:9b") : 
+    response = requests.post("http://localhost:11434/api/generate",
+                             json={"model":MODEL,
+                                   "prompt":prompt,
+                                   "stream":False})
     if response.status_code != 200 : 
         return "Error Calling LLM"
     return response.json().get("response")
