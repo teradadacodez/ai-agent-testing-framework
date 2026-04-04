9616302c8463ea389366e06d7069599019cb71c4
Tanmay <tanmayjain10304@gmail.com>
2026-04-05 00:11:19 +0530

    basic logging and added list_active_models() function

--- src/llm.py
+++ src/llm.py
@@ -4,6 +4,7 @@ def generator_llm(prompt,MODEL = "llama3.2") :
                              json = {"model":MODEL,
                                      "prompt":prompt,
                                      "stream":False})
+    print(f"Chosen Generator model is {MODEL}")
     if response.status_code != 200 : 
         return "Error Calling LLM"
     return response.json().get("response")
@@ -13,6 +14,17 @@ def evaluator_llm(prompt,MODEL = "qwen3.5:9b") :
                              json={"model":MODEL,
                                    "prompt":prompt,
                                    "stream":False})
+    print(f"Chosen Evaluator model is {MODEL}")
     if response.status_code != 200 : 
         return "Error Calling LLM"
     return response.json().get("response")
+
+def list_active_models() : 
+    model_list = requests.get("http://localhost:11434/api/tags").json().get("models")
+    i = 1
+    print("Active models are : ")
+    for model in model_list : 
+        print(f"{i}. {model["model"]}")
+        print(f"\t-parameters = {model["details"]["parameter_size"]}")
+        print(f"\t-size on disk = {model["size"]} bytes")
+        i+=1
