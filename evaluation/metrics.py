41d338e252f2ce696dc8c93ee3b3d38642096615
Tanmay <tanmayjain10304@gmail.com>
2026-04-12 06:17:54 +0530

    added clean_up_ utility

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,25 +1,28 @@
 import json
 
-# mapping id to categories
-with open("data/results.json",encoding="utf-8") as f :
-    res = json.load(f)
-id_to_cat = {int(r["id"]): r["category"] for r in res}
+# utility function, ending with an underscore '_'
+def clean_up_() :
+    # mapping id to categories
+    with open("data/results.json",encoding="utf-8") as f :
+        res = json.load(f)
+    id_to_cat = {int(r["id"]): r["category"] for r in res}
 
-# dropping invalid IDs (cleanup)
-valid_evaluations = []
-with open("data/judgements.json",encoding="utf-8") as f :
-    evaluations = json.load(f)
-for ev in evaluations : 
-    if int(ev["ID"]) in id_to_cat : 
-        valid_evaluations.append(ev)
-    else : 
-        print(f"Dropping ID = {ev['ID']}")
+    # dropping invalid IDs (cleanup)
+    valid_judgements = []
+    with open("data/judgements.json",encoding="utf-8") as f :
+        evaluations = json.load(f)
+    for ev in evaluations : 
+        if int(ev["ID"]) in id_to_cat : 
+            valid_judgements.append(ev)
+        else : 
+            print(f"Dropping ID = {ev['ID']}")
 
-with open("data/judgements.json","w",encoding="utf-8") as f :
-    json.dump(valid_evaluations,f,indent=4)
+    with open("data/judgements.json","w",encoding="utf-8") as f :
+        json.dump(valid_judgements,f,indent=4)
 
-def compute_metrics(evaluations) : 
-    total = len(evaluations)
+def compute_overall_metrics(judgements) : 
+    clean_up_()
+    total = len(judgements)
 
     if total == 0 : 
         return {"accuracy_score":0, "relevance_score":0, "safety_score":0, "avg_score":0}
@@ -28,7 +31,7 @@ def compute_metrics(evaluations) :
     relevance_sum = 0
     safety_sum = 0
 
-    for e in evaluations : 
+    for e in judgements : 
         correctness_sum += e["correctness"]/10  # normalize !
         relevance_sum += e["relevance"]/10
         safety_sum += e["safety"]/10
@@ -47,14 +50,16 @@ def compute_metrics(evaluations) :
 
 # print(id_to_cat)
 
-def category_metrics(evaluations,id_to_cat=id_to_cat) :
+def compute_category_metrics(judgements,results) :
+    clean_up_()
+    id_to_cat = {int(r["id"]): r["category"] for r in results}
     categories = {}
-    for ev in evaluations : 
+    for ev in judgements : 
         cat = id_to_cat[ev["ID"]]
         if cat not in categories : 
             categories[cat] = []
         categories[cat].append(ev)
     category_scores = {}
     for cat, item in categories.items() : 
-        category_scores[cat] = compute_metrics(item)
+        category_scores[cat] = compute_overall_metrics(item)
     return category_scores
